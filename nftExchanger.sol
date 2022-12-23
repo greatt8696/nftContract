4e339c2d45a0a086356cad0c33e128102c04fafa
greatt8696 <greatt8696@gmail.com>
2022-12-23 21:12:25 +0900

    123

--- nftExchanger.sol
+++ nftExchanger.sol
@@ -18,89 +18,83 @@ contract NftExchanger {
         uint256 tokenId;
         uint256 price;
     }
+
     mapping(string => address) public nftList;
 
     mapping(address => mapping(uint256 => SaleInfo)) private _sales;
 
-    function transferFrom(
-        string memory nftKey,
-        address from,
-        address to,
-        uint256 tokenId
-    ) public returns (bool) {
-        (bool success, bytes memory data) = nftList[nftKey].call(
-            abi.encodeWithSignature(
-                "transferFrom(address,address,uint256)",
-                from,
-                to,
-                tokenId
-            )
-        );
-        emit CheckId(nftList[nftKey]);
-        emit TransferFrom(nftKey, from, to, tokenId);
-        return success;
-    }
+    // function transferFrom(
+    //     string memory nftnftName,
+    //     address from,
+    //     address to,
+    //     uint256 tokenId
+    // ) public returns (bool) {
+    //     (bool success, bytes memory data) = nftList[nftnftName].call(
+    //         abi.encodeWithSignature(
+    //             "transferFrom(address,address,uint256)",
+    //             from,
+    //             to,
+    //             tokenId
+    //         )
+    //     );
+    //     emit CheckId(nftList[nftnftName]);
+    //     emit TransferFrom(nftnftName, from, to, tokenId);
+    //     return success;
+    // }
 
     function makeTransfer(
-        string memory key,
+        string memory nftName,
         address from,
         address to,
         uint256 tokenId
     ) public {
-        IERC721(nftList[key]).transferFrom(from, to, tokenId);
+        IERC721(nftList[nftName]).transferFrom(from, to, tokenId);
     }
 
-    function addNftList(string memory key, address CA) public {
-        nftList[key] = CA;
+    function addNftList(string memory nftName, address CA) public {
+        nftList[nftName] = CA;
     }
 
-    function getNftList(string memory key) public view returns (address) {
-        return nftList[key];
+    function getNftList(string memory nftName) public view returns (address) {
+        return nftList[nftName];
     }
 
     function applySale(
-        string memory nftKey,
+        string memory nftnftName,
         uint256 tokenId,
         uint256 price
     ) public {
         require(
-            IERC721(nftList[nftKey]).ownerOf(tokenId) == msg.sender,
+            IERC721(nftList[nftnftName]).ownerOf(tokenId) == msg.sender,
             "nft owner != msg.sender"
         );
         address owner = msg.sender;
-        _sales[nftList[nftKey]][tokenId] = SaleInfo(owner, tokenId, price);
+        _sales[nftList[nftnftName]][tokenId] = SaleInfo(owner, tokenId, price);
         emit ApplySale(owner, tokenId, price);
     }
 
-    function getSales(string memory nftKey, uint256 tokenId)
-        public
-        view
-        returns (
-            address,
-            uint256,
-            uint256
-        )
-    {
-        SaleInfo memory _sale = _sales[nftList[nftKey]][tokenId];
+    function getSales(
+        string memory nftnftName,
+        uint256 tokenId
+    ) public view returns (address, uint256, uint256) {
+        SaleInfo memory _sale = _sales[nftList[nftnftName]][tokenId];
         address _owner = _sale.onwer;
         uint256 _tokenId = _sale.tokenId;
         uint256 _price = _sale.price;
-
         return (_owner, _tokenId, _price);
     }
 
-    function buySale(string memory nftKey, uint256 tokenId) public payable {
+    function buySale(string memory nftnftName, uint256 tokenId) public payable {
         (address _owner, uint256 _tokenId, uint256 _price) = getSales(
-            nftKey,
+            nftnftName,
             tokenId
         );
         require(_price == msg.value);
         address payable seller = payable(_owner);
         address buyer = msg.sender;
         seller.transfer(msg.value);
-        // transferFrom(nftKey, seller, buyer, tokenId);
-        makeTransfer(nftKey, _owner, buyer, tokenId);
-        delete _sales[nftList[nftKey]][_tokenId];
+        makeTransfer(nftnftName, _owner, buyer, tokenId);
+        delete _sales[nftList[nftnftName]][_tokenId];
         emit Sale(buyer, seller, _tokenId, _price);
     }
 }
