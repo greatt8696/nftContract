6f2e3b2cafff988f6bbdcf82a441636aab09fb0b
greatt8696 <greatt8696@gmail.com>
2022-12-24 22:45:14 +0900

    123

--- RandomNumber.sol
+++ RandomNumber.sol
@@ -1,18 +1,48 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract RandomNumber {
-    uint256 public randomNumber;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
     uint256 public max;
     uint256 public start;
 
+    uint256[] public tokenIds;
+
+    event Check(
+        uint256 _tokenId,
+        uint256 max,
+        uint256 start,
+        uint256[] tokenIds
+    );
+
     constructor() {
-        max = 50;
+        max = 10;
+        start = getRandomNumber();
     }
 
-    function getRandomNumber() public returns (uint256) {
-        uint256 rand = uint256(keccak256(abi.encodePacked(msg.sender, block.number))) % max;
+    function getRandomNumber() public view returns (uint256) {
+        uint256 rand = uint256(
+            keccak256(
+                abi.encodePacked(msg.sender, block.number, block.timestamp)
+            )
+        ) % max;
         // uint256 rand = uint256(keccak256(abi.  (msg.sender, block.number)));
         return rand;
+    }
+
+    function mintNft() public returns (uint256) {
+        require(_tokenIdCounter.current() < max, "max mint");
+        uint256 tokenId = ((start + _tokenIdCounter.current()) % max) + 1;
+        _tokenIdCounter.increment();
+        tokenIds.push(tokenId);
+        emit Check(tokenId, max, start, tokenIds);
+        return tokenId;
+    }
+
+    function getCounter() public view returns (uint256) {
+        return _tokenIdCounter.current();
     }
 }
